f015a7d2eab3a3e3ecfa49a2008bef4f2473ff0c
Maycon <maycon@yahoo.com.br>
2015-09-24 09:21:47 -0300

    Alterando textos do banner.

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,6 @@
 var banners =
 ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
-var bannerAtual = 0;
+var bannerAtual = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
 function trocaBanner() {
 bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector(’h2#mensagem’).textContent =
